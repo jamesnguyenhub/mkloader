1ea07b501a83218b788042f13c61e22c89be963e
Tuyen Nguyen <nntuyen92@gmail.com>
2017-03-10 09:19:17 +0700

    Fix memory leak

--- app/src/main/java/com/tuyenmonkey/sample/MainActivity.java
+++ app/src/main/java/com/tuyenmonkey/sample/MainActivity.java
@@ -2,20 +2,11 @@ package com.tuyenmonkey.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_loader2);
-
-    final Button btOpen = (Button)findViewById(R.id.btOpen);
-    btOpen.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        startActivity(TestActivity.getCallingIntent(MainActivity.this));
-      }
-    });
+    setContentView(R.layout.activity_loader1);
   }
 }
