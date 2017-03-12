366e9302727ad9f22e7468262d75156f13f239ce
Tuyen Nguyen <nntuyen92@gmail.com>
2017-03-13 04:07:31 +0700

    Init balloon type

---
+++ library/src/main/java/com/tuyenmonkey/mkloader/type/Balloon.java
@@ -0,0 +1,21 @@
+package com.tuyenmonkey.mkloader.type;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by Tuyen Monkey on 3/13/17.
+ */
+
+public class Balloon extends LoaderView {
+  @Override public void initializeObjects() {
+
+  }
+
+  @Override public void setUpAnimation() {
+
+  }
+
+  @Override public void draw(Canvas canvas) {
+
+  }
+}
